5084a4cf6604758dfc5cd26552f9c6d6f8ae7df6
maxipavolini <86308653+maxipavolini@users.noreply.github.com>
2023-02-02 16:52:53 -0300

    feat(email): send email after register and endpoint for confirm email done

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,22 +1,29 @@
-import { loginType } from './types/login.type';
-import { CreateUserDto } from './../user/dto/create-user.dto';
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
-import { JwtService } from '@nestjs/jwt';
-import { UserService } from '../user/user.service';
-import * as bcrypt from 'bcrypt';
-import { LoginDto } from './dto/login.dto';
+import { loginType } from "./types/login.type";
+import { CreateUserDto } from "./../user/dto/create-user.dto";
+import {
+  BadRequestException,
+  HttpException,
+  HttpStatus,
+  Injectable,
+} from "@nestjs/common";
+import { JwtService } from "@nestjs/jwt";
+import { UserService } from "../user/user.service";
+import * as bcrypt from "bcrypt";
+import { LoginDto } from "./dto/login.dto";
+import { ConfigService } from "@nestjs/config";
 
 @Injectable()
 export class AuthService {
   constructor(
     private userService: UserService,
     private jwtService: JwtService,
+    private readonly configService: ConfigService
   ) {}
 
   async validateUser(
     email: string,
-    pass: string,
-  ): Promise<Omit<CreateUserDto, 'password'>> {
+    pass: string
+  ): Promise<Omit<CreateUserDto, "password">> {
     const user = await this.userService.findByEmail(email);
     const isValid = await bcrypt.compare(pass, user.password);
     if (user && isValid) {
@@ -30,7 +37,7 @@ export class AuthService {
     const user = await this.validateUser(loginDto.email, loginDto.password);
 
     if (!user) {
-      throw new HttpException('Invalid credentials', HttpStatus.UNAUTHORIZED);
+      throw new HttpException("Invalid credentials", HttpStatus.UNAUTHORIZED);
     }
 
     const payload = { email: user.email, sub: user.id };
@@ -40,5 +47,35 @@ export class AuthService {
       user: payload,
       token: access_token,
     };
+  }
+
+  async confirmEmail(token: string): Promise<{ message: string }> {
+    const email = await this.decodeToken(token);
+    const user = await this.userService.findByEmail(email);
+
+    if (user.isActive && user.isActive === true) {
+      throw new BadRequestException("Email already confirmed");
+    }
+    await this.userService.activateUser(email);
+
+    return { message: "Email confirmed successfully" };
+  }
+
+  async decodeToken(token: string) {
+    try {
+      const payload = await this.jwtService.verify(token, {
+        secret: this.configService.get("JWT_SECRET"),
+      });
+
+      if (typeof payload === "object" && "email" in payload) {
+        return payload.email;
+      }
+      throw new BadRequestException();
+    } catch (error) {
+      if (error?.name === "TokenExpiredError") {
+        throw new BadRequestException("Email confirmation token expired");
+      }
+      throw new BadRequestException("Bad confirmation token");
+    }
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,48 +1,51 @@
-import { ChangePasswordDto } from './dto/change-password.dto';
-import { CreateUserDto } from './dto/create-user.dto';
+import { EmailService } from "./../email/email.service";
+import { ChangePasswordDto } from "./dto/change-password.dto";
+import { CreateUserDto } from "./dto/create-user.dto";
 import {
   BadRequestException,
   ConflictException,
   Injectable,
   NotFoundException,
-} from '@nestjs/common';
-import { Repository } from 'typeorm';
-import { User } from './entity/user.entity';
-import { InjectRepository } from '@nestjs/typeorm';
-const bcrypt = require('bcrypt');
+} from "@nestjs/common";
+import { Repository } from "typeorm";
+import { User } from "./entity/user.entity";
+import { InjectRepository } from "@nestjs/typeorm";
+const bcrypt = require("bcrypt");
 
 @Injectable()
 export class UserService {
   constructor(
     @InjectRepository(User) private readonly userRepository: Repository<User>,
+    private readonly emailService: EmailService
   ) {}
 
   async create(userDto: CreateUserDto): Promise<User> {
     const { email } = userDto;
     const user = await this.userRepository.findOne({ where: { email: email } });
     if (user) {
-      throw new ConflictException('Email already in use');
+      throw new ConflictException("Email already in use");
     }
     const newUser = this.userRepository.create(userDto);
     await newUser.hashPassword();
+    await this.emailService.sendVerificationMail(userDto.email, userDto.name);
     return this.userRepository.save(newUser);
   }
 
   async findById(id: number): Promise<CreateUserDto> {
     const user = await this.userRepository.findOne({ where: { id: id } });
-    if (!user) throw new NotFoundException('User not found');
+    if (!user) throw new NotFoundException("User not found");
     return user;
   }
 
   async findByEmail(email: string): Promise<CreateUserDto> {
     const user = await this.userRepository.findOne({ where: { email: email } });
-    if (!user) throw new NotFoundException('User not found');
+    if (!user) throw new NotFoundException("User not found");
     return user;
   }
 
   async resetPassword(email: string): Promise<ChangePasswordDto> {
     if (!email) {
-      throw new BadRequestException('Email is required');
+      throw new BadRequestException("Email is required");
     }
     const user = await this.findByEmail(email);
     // send email to enter the new password
@@ -51,17 +54,17 @@ export class UserService {
 
   async changePassword(
     id: number,
-    changePasswordDto: ChangePasswordDto,
+    changePasswordDto: ChangePasswordDto
   ): Promise<void> {
     const user = await this.findById(id);
 
     const pwMatch = await bcrypt.compare(
       changePasswordDto.currentPassword,
-      changePasswordDto.newPassword,
+      changePasswordDto.newPassword
     );
 
     if (!pwMatch) {
-      throw new Error('Incorrect password');
+      throw new Error("Incorrect password");
     }
 
     user.password = await bcrypt.hash(changePasswordDto.newPassword, 10);
@@ -73,9 +76,18 @@ export class UserService {
       where: { id: parseInt(id) },
     });
     if (!user) {
-      throw new NotFoundException('User not found');
+      throw new NotFoundException("User not found");
     }
     user.isActive = false;
     await this.userRepository.save(user);
+  }
+
+  async activateUser(email: string) {
+    return this.userRepository.update(
+      { email },
+      {
+        isActive: true,
+      }
+    );
   }
 }
