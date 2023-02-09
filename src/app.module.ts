4e6925a7fdf681c886b4eae76ab4f179565dc6eb
maxipavolini <86308653+maxipavolini@users.noreply.github.com>
2023-02-09 17:23:27 -0300

    feat(rateLimiter): basic throttler config added and packages updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,25 +1,41 @@
-import { dataSourceOptions } from '../db/data-source';
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
-import { ConfigModule } from '@nestjs/config';
-import { AuthModule } from './auth/auth.module';
-import { EmailModule } from './email/email.module';
+import { dataSourceOptions } from "../db/data-source";
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { UserModule } from "./user/user.module";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { AuthModule } from "./auth/auth.module";
+import { EmailModule } from "./email/email.module";
+import { ThrottlerModule, ThrottlerGuard } from "@nestjs/throttler";
+import { APP_GUARD } from "@nestjs/core";
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: './.env',
+      envFilePath: "./.env",
       isGlobal: true,
     }),
     TypeOrmModule.forRoot(dataSourceOptions),
+    ThrottlerModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        ttl: config.get("THROTTLE_TTL"),
+        limit: config.get("THROTTLE_LIMIT"),
+      }),
+    }),
     UserModule,
     AuthModule,
     EmailModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
